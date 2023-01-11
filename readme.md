c4baf855df08b8f9d46a7758472d206840590bf3
tdarlic <tomislav.darlic@gmail.com>
2023-01-11 21:49:04 +0100

    Initial commit
    Added already created keymap

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Default keymap for Redox Wireless
+Added croatian letters on _ADJUST_ (3) layer
---
+++ rules.mk
@@ -0,0 +1,7 @@
+TAP_DANCE_ENABLE = no # Enable the tap dance feature.
+#COMBO_ENABLE = yes
+#LEADER_ENABLE = yes
+CONSOLE_ENABLE = no
+#LTO_ENABLE = yes # link time optimizations
+#UNICODE_ENABLE = yes
+UNICODEMAP_ENABLE = yes
